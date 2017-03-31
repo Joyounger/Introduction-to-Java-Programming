f7d7c258ac141815c91c2b9d4deb2427d49cf2f8
Joyounger <942510346@qq.com>
2017-03-31 15:02:54 +0800

    整数栈类StackOfIntegers的实现
    Signed-off-by: Joyounger <942510346@qq.com>

--- chap7/StackOfIntegers.java
+++ chap7/StackOfIntegers.java
@@ -16,11 +16,31 @@ public class StackOfIntegers {
 	}
 
 	public int push(int value) {
-		if (size >= elements.length) {
-			
+		if (size >= elements.length) {  // 如果堆栈已满
+			int[] tmp = new int[elements.length * 2];
+			System.arraycopy(elements, 0, tmp, 0, elements.length);
+			elements = tmp; //将新数组的引用赋值给栈中的当前数组
 		}
+
+		return elements[size++] = value;
 	}
 
 
+	public int pop() {
+		return elements[--size];
+	}
+
+	// return the top element from the stack
+	public int peek() {
+		return elements[size - 1];
+	}
+
+	public boolean empty() {
+		return size == 0;
+	}
+
+	public int getSize() {
+		return size;
+	}
 
 }
